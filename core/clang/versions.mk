bbacb4e552943ea77f2d2c1bd0a411c9311ecb38
ZION959 <zionmtran@gmail.com>
2016-11-18 01:05:31 -0700

    update clang version

--- core/clang/versions.mk
+++ core/clang/versions.mk
@@ -1,5 +1,7 @@
 ## Clang/LLVM release versions.
+LLVM_PREBUILTS_VERSION := clang-3859424
+LLVM_PREBUILTS_BASE := prebuilts/clang/host
 
-LLVM_RELEASE_VERSION := 3.8
-LLVM_PREBUILTS_VERSION ?= clang-2690385
-LLVM_PREBUILTS_BASE ?= prebuilts/clang/host
+## RenderScript-specific tools
+RS_LLVM_PREBUILTS_VERSION := clang-2690385
+RS_LLVM_PREBUILTS_BASE := prebuilts/clang/host
